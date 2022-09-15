8e1b31d6e51c32218ca138203de5934fd28776ee
katoyutaka <kyupi13@gmail.com>
2022-09-15 14:01:54 +0900

    work16と16_02の修正後プッシュ

--- work16.php
+++ work16.php
@@ -10,15 +10,14 @@
             <form method = "get" action = "work16_02.php">
                 <input type="text" name="name_box">
                 <br>
-                <p>選択肢01</p><input type="checkbox" name="select01[]">
-                <p>選択肢02</p><input type="checkbox" name="select02[]">
-                <p>選択肢03</p><input type="checkbox" name="select03[]">
+                <p>選択肢01</p><input type="checkbox" name="select[]" value="選択肢01">
+                <p>選択肢02</p><input type="checkbox" name="select[]" value="選択肢02">
+                <p>選択肢03</p><input type="checkbox" name="select[]" value="選択肢03">
                 <br>
                 <input type="submit" value="送信">
             </form>
         
     </body>
 
-    <!-- work16からスタート！！！ -->
     
 </html>
--- work16_02.php
+++ work16_02.php
@@ -9,14 +9,13 @@
     <body>
         <div>フォームに入力した内容を表示する。</div>
             <?php
-                if(isset($_GET["name_box"])){
                     print "テキストボックスの入力した内容：".htmlspecialchars($_GET["name_box"],ENT_QUOTES,"UTF-8");
-                    print "チェックボックスのボックスの入力した内容：".htmlspecialchars($_GET["select01[]"],ENT_QUOTES,"UTF-8");
-                    print "チェックボックスのボックスの入力した内容：".htmlspecialchars($_GET["select02[]"],ENT_QUOTES,"UTF-8");
-                    print "チェックボックスのボックスの入力した内容：".htmlspecialchars($_GET["select03[]"],ENT_QUOTES,"UTF-8");
-                } else {
-                    print "入力されていません";
-                }
+                    print "<br>";
+                    print "チェックボックスのボックスの入力した内容：".htmlspecialchars($_GET["select"][0],ENT_QUOTES,"UTF-8");
+                    print "<br>";
+                    print "チェックボックスのボックスの入力した内容：".htmlspecialchars($_GET["select"][1],ENT_QUOTES,"UTF-8");
+                    print "<br>";
+                    print "チェックボックスのボックスの入力した内容：".htmlspecialchars($_GET["select"][2],ENT_QUOTES,"UTF-8");
             ?>
     
     </body>
