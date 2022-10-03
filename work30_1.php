e1825c408f0fb3da74d309cb855246c1b4c829cb
katoyutaka <kyupi13@gmail.com>
2022-10-03 11:04:40 +0900

    10/3

--- work30_1.php
+++ work30_1.php
@@ -1,84 +1,76 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>WORK30_1</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>WORK30_1</title>
 
- <style>
-   h1{
-    font-size:20px; 
-   }
+        <style>
+                h1 {
+                    font-size:20px; 
+                }
 
-   p{
-    font-size:18px; 
-   }
+                p {
+                    font-size:18px; 
+                }
 
-   .box1,.box2,.box3,.box4,.box5,.box6{
-    color: #fff;
-    font-weight: bold; 
-    text-align: center; 
-    line-height: 100px;
-    background-color: #3CB371;
-    margin: 20px; 
-    width: 100px;
-    }
+                .box1,.box2,.box3,.box4,.box5,.box6 {
+                    color: #fff;
+                    font-weight: bold; 
+                    text-align: center; 
+                    line-height: 100px;
+                    background-color: #3CB371;
+                    margin: 20px; 
+                    width: 100px;
+                }
 
-   .container{
-       flex-wrap:wrap;
-       width:500px;
+                .container {
+                    flex-wrap:wrap;
+                    width:500px;
+                }
+        </style>
 
-   }
-
-</style>
-
-</head>
-
-<body>
-
-<form method="post" action="sample.php" enctype="multipart/form-data">
- <h1>画像投稿</h1>
- <br>
- <p>画像名：<input type="text" name="input_data"></p>
-
- <p>画像 ：<input type="file" name="upload_image"></p>
-
-<p><input type="submit" value="画像投稿"></p>
-
-</form>
-
-<a href="work30_1.php">画像一覧ページへ</a>
-
-
-<div class="container">
-    <div class="box1">
-     <p>ここに文章</p>
-    </div>
-
-    <div class="box2">
-     <p>ここに文章</p>
-    </div>
-
-    <div class="box3">
-     <p>ここに文章</p>
-    </div>
-
-    <div class="box4">
-     <p>ここに文章</p>
-    </div>
-
-    <div class="box5">
-     <p>ここに文章</p>
-    </div>
-
-    <div class="box6">
-     <p>ここに文章</p>
-    </div>
-</div>
-
-
-　
+    </head>
 
 
 
-</body>
+    <body>
+
+        <form method="post" action="work30_2.php" enctype="multipart/form-data">
+            <h1>画像投稿</h1>
+            <br>
+            <p>画像名：<input type="text" name="input_data"></p>
+            <p>画像 :<input type="file" name="upload_image"></p>
+            <p><input type="submit" value="画像投稿"></p>
+        </form>
+
+        <a href="work30_2.php">画像一覧ページへ</a>
+
+
+        <div class="container">
+            <div class="box1">
+            <p>ここに文章</p>
+            </div>
+
+            <div class="box2">
+            <p>ここに文章</p>
+            </div>
+
+            <div class="box3">
+            <p>ここに文章</p>
+            </div>
+
+            <div class="box4">
+            <p>ここに文章</p>
+            </div>
+
+            <div class="box5">
+            <p>ここに文章</p>
+            </div>
+
+            <div class="box6">
+            <p>ここに文章</p>
+            </div>
+        </div>
+
+    </body>
 </html>
--- work30_2.php
+++ work30_2.php
@@ -1,52 +1,50 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>WORK30_2</title>
-<style>
-   h1{
-    font-size:20px; 
-   }
+    <head>
+        <meta charset="UTF-8">
+        <title>WORK30_2</title>
 
-   p{
-    font-size:18px; 
-</style>
-</head>
+        <style>
+            h1{
+                font-size:20px; 
+            }
 
-
-
-<body>
-
-<form>
-<h1>画像一覧</h1>
-
-<a href="work30_2.php">画像投稿ページへ</a>
-
-
-<?php
-if(!isset($_FILES['upload_image'])):
-    print 'ファイルが送信されていません。';
-				exit;
-endif;
-
-print $_FILES['upload_image']['name'];
-
-$save = 'img/'.basename($_FILES['upload_image']['name']);
-
-        if(move_uploaded_file($_FILES['upload_image']['tmp_name'],$save)):
-    									print "かんりょう";
-							else:
-    									print "しっぱい";
-							endif;
-
-?>
-</form>
+            p{
+            font-size:18px;
+            } 
+        </style>
+        
+        
+    </head>
 
 
 
+    <body>
+
+        <form>
+        <h1>画像一覧</h1>
+
+        <?php
+            if(!isset($_FILES['upload_image'])):
+                print 'ファイルが送信されていません。';
+                exit;
+            endif;
+
+            print $_FILES['upload_image']['name'];
+
+            $save = 'img/'.basename($_FILES['upload_image']['name']);
+
+                    if(move_uploaded_file($_FILES['upload_image']['tmp_name'],$save)){
+                        print "完了";
+                    }else{
+                        print "失敗";
+                    }
+
+        ?>
+        </form>
+
+        <a href="work30_1.php">画像投稿ページへ</a>
 
 
-
-
-</body>
+    </body>
 </html>
