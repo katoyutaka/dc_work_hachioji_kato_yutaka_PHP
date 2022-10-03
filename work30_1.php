a8f31de6b9957db94c1135b4b0c510dd035401e3
katoyutaka <kyupi13@gmail.com>
2022-10-03 12:01:54 +0900

    Update work30_1.php

--- work30_1.php
+++ work30_1.php
@@ -13,19 +13,32 @@
                     font-size:18px; 
                 }
 
-                .box1,.box2,.box3,.box4,.box5,.box6 {
+
+                .box1,
+                .box2,
+                .box3,
+                .box4,
+                .box5,
+                .box6 {
                     color: #fff;
                     font-weight: bold; 
-                    text-align: center; 
-                    line-height: 100px;
-                    background-color: #3CB371;
-                    margin: 20px; 
-                    width: 100px;
+                    width: 31%;
+                    border:1px solid #b7b7b7;
+                    margin-right:5px;
+
                 }
 
                 .container {
-                    flex-wrap:wrap;
-                    width:500px;
+                    display:flex;
+                    flex-wrap: wrap;
+                    width:400px;
+                    background-color: red;
+                }
+
+                .container p {
+                    text-align: center;
+                    font-size:10px;
+                    
                 }
         </style>
 
@@ -37,9 +50,8 @@
 
         <form method="post" action="work30_2.php" enctype="multipart/form-data">
             <h1>画像投稿</h1>
-            <br>
             <p>画像名：<input type="text" name="input_data"></p>
-            <p>画像 :<input type="file" name="upload_image"></p>
+            <p>画像 : <input type="file" name="upload_image"></p>
             <p><input type="submit" value="画像投稿"></p>
         </form>
 
@@ -49,26 +61,32 @@
         <div class="container">
             <div class="box1">
             <p>ここに文章</p>
+            <img class="introduce-image" src="絶対パス付きファイル名" alt="">
             </div>
 
             <div class="box2">
             <p>ここに文章</p>
+            <img class="introduce-image" src="絶対パス付きファイル名" alt="">
             </div>
 
             <div class="box3">
             <p>ここに文章</p>
+            <img class="introduce-image" src="絶対パス付きファイル名" alt="">
             </div>
 
             <div class="box4">
             <p>ここに文章</p>
+            <img class="introduce-image" src="絶対パス付きファイル名" alt="">
             </div>
 
             <div class="box5">
             <p>ここに文章</p>
+            <img class="introduce-image" src="絶対パス付きファイル名" alt="">
             </div>
 
             <div class="box6">
             <p>ここに文章</p>
+            <img class="introduce-image" src="絶対パス付きファイル名" alt="">
             </div>
         </div>
 
