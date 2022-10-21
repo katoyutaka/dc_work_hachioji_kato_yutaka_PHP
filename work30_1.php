d7bf64359b41ff8ff22506e702fb916c94bfe4b1
katoyutaka <kyupi13@gmail.com>
2022-10-21 14:48:38 +0900

    Update work30_1.php

--- work30_1.php
+++ work30_1.php
@@ -7,7 +7,7 @@
 
         // $image_id = 7777;
         // $image_name = $input_data;
-
+        $public_flg = 0;
         $create_date = date('Ymd');
         $update_date = date('Ymd');
         
@@ -35,8 +35,13 @@
     $upload_image_tmp_name = htmlspecialchars($_FILES['upload_image']['tmp_name'], ENT_QUOTES, 'UTF-8');
   }
 
-?>
+  $image_path ='';		
+  if(isset($_FILES['upload_image']['name'])){
+    $image_path = 'https://portfolio.dc-itex.com/hachioji/0001/img/'.htmlspecialchars($_FILES['upload_image']['name'], ENT_QUOTES, 'UTF-8');
+  }
 
+
+?>
 
 
 
@@ -135,24 +140,9 @@
 
         <form method="post" action="work30_1.php" enctype="multipart/form-data">
             <h1>画像投稿</h1>
-
+ 
             <!-- エラーメッセージや登録・更新完了メッセージ表示部分 -->
             <?php
-                   //「画像名」は半角英数字以外はエラー表示
-                   if(!preg_match("/^[a-zA-Z0-9]+$/",$input_data) || $input_data == ""){
-                        $str = "「画像名」が半角英数字以外の形式になっています。";
-                        print "<span class='msg'>$str</span><br>";
-                    //exitをつけると最初からNG状態の表示になり、つけないとマッチ機能が働かない？
-                        // exit();
-                   }
-
-                   //「画像」はjpeg,png以外はエラー表示
-                   if((!preg_match("/\.png$/",$upload_image_name)||!preg_match("/\.jpeg$/",$upload_image_name)) || $upload_image == ""){
-                        $str = "「画像」の投稿形式（拡張子）が「JPEG」「PNG」以外になっています。";
-                        print "<span class='msg'>$str</span><br>";
-                    //  //exitをつけると最初からNG状態の表示になり、つけないとマッチ機能が働かない？
-                    //   exit();
-                   }
 
 
                 //    データベース（phpmyadmin）にフォームからデータ送信
@@ -170,10 +160,32 @@
                     
                     //SQL文が送れない（データベースにinsert intoしても反映されない事象が起きた。調査した結果、カラム名「image_id」とかに""を付けてるとダメ。なにも付けないこと！）
                     //「 '$image_name'」の「''」を付けないと送信出来るときと出来ない時がある。→変数と文字列なので連結しないと。しかし「'$image_name'」だけ他と連結がちがうのにOKなのはなぜ？
-
+                    //→→原因判明した！！！まず、$input_dataと$image_pathは文字列なのでシングルまたはダブルクォーテーションをつけなければならないが、$input_dataは先頭でダブルつかっているので、ダブルだと反応してしまうので、シングルでなければならない。
+                    //ただし、連結は不要なのか？
                     //update,delete,insertの時は、トランザクション・ロールバック機能をつける。
                         $db->begin_transaction();
-                        $insert = "INSERT INTO gallery ( image_name, public_flg, create_date, update_date) VALUES ('$input_data', ".$public_flg.", ".$create_date." , ".$update_date.");";
+
+
+                        //「画像名」は半角英数字以外または空白はエラー表示
+                        if(!preg_match("/^[a-zA-Z0-9]+$/",$input_data) || $input_data == ""){
+                            $str = "「画像名」が半角英数字以外の形式になってるか、入力がされていません。";
+                            print "<span class='msg'>$str</span><br>";
+                        //exitをつけると最初からNG状態の表示になり、つけないとマッチ機能が働かない？
+                            
+                        }
+
+                        //「画像」はjpeg,png以外または空白はエラー表示
+                        if((!preg_match("/\.png$/",$upload_image_name)||!preg_match("/\.jpeg$/",$upload_image_name)) || $upload_image == ""){
+                                $str = "「画像」の投稿形式（拡張子）が「JPEG」「PNG」以外の形式になっているか、選択されていません。";
+                                print "<span class='msg'>$str</span><br>";
+                            //  //exitをつけると最初からNG状態の表示になり、つけないとマッチ機能が働かない？
+                            // exit();
+                        }
+
+
+
+
+                        $insert = "INSERT INTO gallery ( image_name, public_flg, create_date, update_date,image_path) VALUES ('$input_data',".$public_flg.",".$create_date.",".$update_date.",'$image_path');";
 
                         //ここにすでにデータベースに投稿データがある場合は投稿できないようにするコードを書きたい!!!→トランザクション・ロールバック機能で対応しした。
                         if($result=$db->query($insert)){
@@ -193,7 +205,9 @@
                             $str = "データベースに既に同じファイル名が存在している為か、その他の理由により更新失敗しました。";
                             print "<span class='msg'>$str</span><br>";
                             $db->rollback();
-                            // var_dump($error_msg); 
+                            var_dump($error_msg);
+                            print_r ($error_msg);
+
                         }
 
 
@@ -235,7 +249,8 @@
                     if($result = $db->query($select)){
                         // 連想配列を取得
                         while ($row = $result->fetch_assoc()) {
-                            $get_img_url = 'https://portfolio.dc-itex.com/hachioji/0001/img/'.$row["image_name"];
+                            $get_img_url = $row["image_path"];
+                            
             ?>                         
                                 <div class="box">
                                     <div class=img-container>
