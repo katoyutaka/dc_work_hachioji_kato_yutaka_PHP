8b1217e637c2675f27c667bccb44cda166ab4365
katoyutaka <kyupi13@gmail.com>
2022-10-27 13:53:25 +0900

    Update work30_1.php

--- work30_1.php
+++ work30_1.php
@@ -1,28 +1,22 @@
-<!-- 変数の各種詳細 -->
+
 <?php
         $host = 'mysql34.conoha.ne.jp'; 
         $login_user = 'bcdhm_hoj_pf0001'; 
         $password = 'Au3#DZ~G';   
         $database = 'bcdhm_hoj_pf0001';
 
-        // $image_id = 7777;
-        // $image_name = $input_data;
         $public_flg = 0;
         $create_date = date('Ymd');
         $update_date = date('Ymd');
-        
         $error_msg=[];
 ?>
 
-<!-- 初期化とXSSを防ぐための「エスケープ処理」 2種類（input_data、upload_image）-->
-<!-- 「 htmlspecialchars」に配列は使えなく、配列から文字列を取り出して使うようにエラーが出る。 -->
+
 <?php
   $input_data ='';		
   if(isset($_POST['input_data'])){
     $input_data = htmlspecialchars($_POST['input_data'], ENT_QUOTES, 'UTF-8');
-}
-
-
+  }
 
 
   $upload_image_name ='';		
@@ -39,7 +33,6 @@
   if(isset($_FILES['upload_image']['name'])){
     $image_path = 'https://portfolio.dc-itex.com/hachioji/0001/img/'.htmlspecialchars($_FILES['upload_image']['name'], ENT_QUOTES, 'UTF-8');
   }
-
 
 ?>
 
@@ -141,80 +134,39 @@
         <form method="post" action="work30_1.php" enctype="multipart/form-data">
             <h1>画像投稿</h1>
 
-            <?php
-  
-
-
-            ?>
  
-            <!-- エラーメッセージや登録・更新完了メッセージ表示部分 -->
             <?php
+                $db = new mysqli($host, $login_user, $password, $database);
+                if($db->connect_error){
+                    print $db->connect_error;
+                    exit();
 
+                }else{
+                    $db->set_charset("utf8");
+                }	
 
-                //    データベース（phpmyadmin）にフォームからデータ送信
-            
-                    $db = new mysqli($host, $login_user, $password, $database);
-                    if($db->connect_error){
-                        print $db->connect_error;
-                        exit();
-
-                    
-
+                $db->begin_transaction();
+                
+                if(!preg_match("/^[a-zA-Z0-9]+$/",$input_data)){
+                    $str = "「画像名」が半角英数字以外の形式になってるか、入力がされていません。";
+                }else{
+                    $file=pathinfo($image_path);
+                    $filetype=$file["extension"];
+                    print $filetype;
+                        
+                    if(!$filetype==="jpeg"||!$filetype==="png"){
+                        $str = "拡張子がJPEGまたはPNG以外の形式になっています。";
+                        print "<span class='msg'>$str</span><br>";
                     }else{
-                        $db->set_charset("utf8");	
-
-                    
-                    //SQL文が送れない（データベースにinsert intoしても反映されない事象が起きた。調査した結果、カラム名「image_id」とかに""を付けてるとダメ。なにも付けないこと！）
-                    //「 '$image_name'」の「''」を付けないと送信出来るときと出来ない時がある。→変数と文字列なので連結しないと。しかし「'$image_name'」だけ他と連結がちがうのにOKなのはなぜ？
-                    //→→原因判明した！！！まず、$input_dataと$image_pathは文字列なのでシングルまたはダブルクォーテーションをつけなければならないが、$input_dataは先頭でダブルつかっているので、ダブルだと反応してしまうので、シングルでなければならない。
-                    //ただし、連結は不要なのか？
-                    //update,delete,insertの時は、トランザクション・ロールバック機能をつける。
-                        $db->begin_transaction();
-
-
-                        //「画像名」は半角英数字以外または空白はエラー表示
-                        if(!preg_match("/^[a-zA-Z0-9]+$/",$input_data) || $input_data == ""){
-                            $str = "「画像名」が半角英数字以外の形式になってるか、入力がされていません。";
-                            print "<span class='msg'>$str</span><br>";
-                            // exit();
-                        //exitをつけると最初からNG状態の表示になり、つけないとマッチ機能が働かない？
-                            
-                        }
-
-
-                        
-                        //絶対付きファイル名から拡張子のみを取り出す方法。
-                        $file=pathinfo($image_path);
-                        $filetype=$file["extension"];
-                        print $filetype;
-                        
-                        //拡張子判定部分（jpeg,png）
-                        if(!$filetype==="jpeg"||!$filetype==="png"){
-                            $str = "拡張子がJPEGまたはPNG以外の形式になっています。";
-                            print "<span class='msg'>$str</span><br>";
-                            // exit();
-                        }
-              
-                        // //「画像」はjpeg,png以外または空白はエラー表示
-                        // if((!preg_match("/\.png$/",$upload_image_name)||!preg_match("/\.jpeg$/",$upload_image_name)) || $upload_image == ""){
-                        //         $str = "「画像」の投稿形式（拡張子）が「JPEG」「PNG」以外の形式になっているか、選択されていません。";
-                        //         print "<span class='msg'>$str</span><br>";
-                        //     //  //exitをつけると最初からNG状態の表示になり、つけないとマッチ機能が働かない？
-                        //     // exit();
-                        // }
-
-
-
-
                         $insert = "INSERT INTO gallery ( image_name, public_flg, create_date, update_date,image_path) VALUES ('$input_data',".$public_flg.",".$create_date.",".$update_date.",'$image_path');";
 
                         //ここにすでにデータベースに投稿データがある場合は投稿できないようにするコードを書きたい!!!→トランザクション・ロールバック機能で対応しした。
                         if($result=$db->query($insert)){
                             $save = 'img/'.basename($upload_image_name);
                             move_uploaded_file($upload_image_tmp_name,$save);
-                        
                         }else{
                             $error_msg[]="実行エラー".$insert;
+                            $db->rollback();
                         }
                         
                         if (count($error_msg) == 0) {
@@ -226,16 +178,16 @@
                             $str = "データベースに既に同じファイル名が存在している為か、その他の理由により更新失敗しました。";
                             print "<span class='msg'>$str</span><br>";
                             $db->rollback();
-                            var_dump($error_msg);
-                            print_r ($error_msg);
 
                         }
+                    }
+                }
 
 
                         
                         // $insert = "INSERT INTO gallery ( image_name, public_flg, create_date, update_date) VALUES (".$input_data.", ".$public_flg.", ".$create_date." , ".$update_date.");";
                   
-                    }
+                    
                 
 
                    //画像のアップロードOK/NGの判定
@@ -243,6 +195,25 @@
                    //$upload_imageが配列であることを宣言しないと、$upload_image['tmp_name']の部分で「Illegal string offset」とエラーが出る。
                    //「Illegal string offset」は「配列でない変数に対して、配列のつもりで値を代入してしまうために起こるエラー」のこと。
                 //    $upload_image=array();
+                                        // //「画像」はjpeg,png以外または空白はエラー表示
+                        // if((!preg_match("/\.png$/",$upload_image_name)||!preg_match("/\.jpeg$/",$upload_image_name)) || $upload_image == ""){
+                        //         $str = "「画像」の投稿形式（拡張子）が「JPEG」「PNG」以外の形式になっているか、選択されていません。";
+                        //         print "<span class='msg'>$str</span><br>";
+                        //     //  //exitをつけると最初からNG状態の表示になり、つけないとマッチ機能が働かない？
+                        //     // exit();
+                        // }
+                                            
+                        //SQL文が送れない（データベースにinsert intoしても反映されない事象が起きた。調査した結果、カラム名「image_id」とかに""を付けてるとダメ。なにも付けないこと！）
+                        //「 '$image_name'」の「''」を付けないと送信出来るときと出来ない時がある。→変数と文字列なので連結しないと。しかし「'$image_name'」だけ他と連結がちがうのにOKなのはなぜ？
+                        //→→原因判明した！！！まず、$input_dataと$image_pathは文字列なのでシングルまたはダブルクォーテーションをつけなければならないが、$input_dataは先頭でダブルつかっているので、ダブルだと反応してしまうので、シングルでなければならない。
+                        //ただし、連結は不要なのか？
+                        //update,delete,insertの時は、トランザクション・ロールバック機能をつける。
+                        // print "<span class='msg'>$str</span><br>";
+                        // exit();
+                        //exitをつけると最初からNG状態の表示になり、つけないとマッチ機能が働かない？
+                        //                         <!-- 初期化とXSSを防ぐための「エスケープ処理」 2種類（input_data、upload_image）-->
+                        // <!-- 「 htmlspecialchars」に配列は使えなく、配列から文字列を取り出して使うようにエラーが出る。 -->
+
 
                    
 
