d299aceca6d5c2e7c178ff73fc302ae9896d33a9
katoyutaka <kyupi13@gmail.com>
2022-12-22 14:54:07 +0900

    12/22

--- work38_1.php
+++ work38_1.php
@@ -1,47 +1,28 @@
-<!-- この課題は入力したIDとパスワードがSQLのデータベースと一致したらログインできて画面が「ログイン（擬似的）が完了しました」に遷移。 -->
-<!-- 不一致や未入力ならばログインできないようにする。かつ、cookie機能を付ける。つまり２つ課題がある。 -->
-<!-- 可能ならば（余力があれば）、初期にidとパスワードをSQLに登録させる「新規登録」の部分も追加したい。 -->
 
 <?php
     session_start();
+
+    if($_flag_a=="on"){
+        print "セッション中です";
+    }
+ 
     if(isset($_POST["logout"])){
 
         $session = session_name();
         $_SESSION = [];
 
         if (isset($_COOKIE[$session])) {
-            // sessionに関連する設定を取得
-            // $paramsの部分は不要では？
-            // $params = session_get_cookie_params();
 
-            // cookie削除
             setcookie($session, '', time() - 30, '/');
             print "<p>ログアウトされました。</p>";
         }
     } else {
-        // ログイン中のユーザーであるかを確認する
         if (isset($_SESSION['login_id'])) {
-            // ログイン中である場合は、top.phpにリダイレクト（転送）する
             header('Location: work38_2.php');
             exit();
         }
     }
 
-
-
-// <!-- ➃ -->
-// <!-- cookie機能がセット（setcookie）があれば「$cookie_confirmation」の変数にcookieでゲットした値を格納する。入力されなかったら、「$cookie_confirmation」の変数は空欄。 --
-if(isset($_COOKIE["cookie_confirmation"]) === TRUE){
-    $cookie_confirmation = "checked";
-} else {
-    $cookie_confirmation = "";
-}
-// <!-- cookie機能がセット（setcookie）があれば「$login_id」の変数にcookieでゲットした値を格納する。入力されなかったら、「$login_id」の変数は空欄。 -->
-if(isset($_COOKIE["login_id"]) === TRUE){
-    $login_id = $_COOKIE["login_id"];
-} else {
-    $login_id = "";
-}
 ?>
 
 
--- work38_2.php
+++ work38_2.php
@@ -1,60 +1,34 @@
 
         <?php
-                $host = 'mysql34.conoha.ne.jp'; 
-                $login_user = 'bcdhm_hoj_pf0001'; 
-                $password = 'Au3#DZ~G';   
-                $database = 'bcdhm_hoj_pf0001';
+            $host = 'mysql34.conoha.ne.jp'; 
+            $login_user = 'bcdhm_hoj_pf0001'; 
+            $password = 'Au3#DZ~G';   
+            $database = 'bcdhm_hoj_pf0001';
 
             session_start();
-            // cookieの保存期間を１日に設定
-            define("EXPIRATION_PERIOD", 1);
-            $cookie_expiration= time()+EXPIRATION_PERIOD*60*60*24;
 
-            // // ➁
-            // // もしチェックボックスがチェックされたら、「$cookie_confirmation」にデータを格納して、チェックされなかったら空欄を格納する。
-            // こちらは$_POSTに値があるならば、、（work37_1は$COOKIEに値があるならば）
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                if(isset($_POST["cookie_confirmation"])){
-                    $cookie_confirmation = $_POST["cookie_confirmation"];
-                } else {
-                    $cookie_confirmation = "";
-                }
-                // もしログインIDが入力されたら、「$login_id」の変数に格納する。
+
                 if(isset($_POST["login_id"])){
-
                     $login_id = $_POST["login_id"];
-                    $_SESSION['login_id'] =  $login_id;
-
-                } else {
-                    $login_id = '';
-                    $_SESSION['err_flg'] = true;
-                }
-                // もしパスワードが入力されたら、「$password_id」の変数に格納する。
+                } 
+            
                 if(isset($_POST["password_id"])){
                     $password_id = $_POST["password_id"];
                 }
             }
 
-
-
-
-            // ➂
-            // 上記で、チェックがあれば（変数「$cookie_confirmation」に値が入れば）、cookie機能つけて、ないなら機能なしでcookie削除。
-            if($cookie_confirmation === "checked"){
-                setcookie("login_id",$login_id,$cookie_expiration);
-                setcookie("cookie_confirmation",$cookie_confirmation,$cookie_expiration);
-            } else {
-                setcookie("login_id","",time()-100);
-                setcookie("cookie_confirmation","",time()-100);
+            if($_flag_a=="on"){
+                print "セッション中です";
             }
-
-            if (!isset($_SESSION['login_id'])) {
-                // ログイン中ではない場合は、try55.phpにリダイレクト（転送）する
-                header('Location: work38_1.php');
-                exit();
-            } else {
-                echo "<p>" . $_SESSION['login_id'] . "さん：ログイン中です。</p>";
-            }
+            // if (isset($_SESSION['login_id'])) {
+            //     echo "<p>" . $_SESSION['login_id'] . "さん：ログイン中です。</p>";
+            // } else {
+            //     header('Location: work38_1.php');
+            //     exit();
+                
+            // }
+    
         ?>
 
 
@@ -79,8 +53,15 @@
                 if($password_id===$row["password"]){
                     print "<p>ログイン（擬似的）が完了しました</p>";
                     print $row["user_name"]."さん、ようこそ！";
-                } else {
-                    print "ログインに失敗しました";
+                    
+                    $_SESSION['login_id'] =  $login_id;
+                    $_flag_a = "on";
+
+                }else{
+                    $login_id = '';
+                    header('Location: work38_1.php');
+                    exit();
+                    
                 }
         }
 
