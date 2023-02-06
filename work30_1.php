9f2959d782ff8d38c701f47fd78e5bcfc9ba8b52
katoyutaka <kyupi13@gmail.com>
2023-02-06 13:59:45 +0900

    Update work30_1.php

--- work30_1.php
+++ work30_1.php
@@ -30,86 +30,85 @@
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         if(isset($_POST["submit"])){
             
+                    $input_data ='';
+                        
+                    if(!empty($_POST['input_data'])){
+                        $input_data = htmlspecialchars($_POST['input_data'], ENT_QUOTES, 'UTF-8');
+                    } else {
+                        $validation_error[]="画像名に問題があるか、未入力です"."<br>";
+                    }
 
-        $input_data ='';
-        	
-        if(!empty($_POST['input_data'])){
-            $input_data = htmlspecialchars($_POST['input_data'], ENT_QUOTES, 'UTF-8');
-        } else {
-            $validation_error[]="画像名に問題があるか、未入力です"."<br>";
-        }
+                    $upload_image_name ='';
+                    $image_path ='';
 
-        $upload_image_name ='';
-        $image_path ='';
+                    if(isset($_FILES['upload_image']['name'])){
+                        $upload_image_name = htmlspecialchars($_FILES['upload_image']['name'], ENT_QUOTES, 'UTF-8');
+                        $image_path =  './img/'.htmlspecialchars($_FILES['upload_image']['name'], ENT_QUOTES, 'UTF-8');
+                    } else {
+                        $validation_error[] = "アップロードファイル名に問題があるか、未選択です"."<br>";
+                    }
 
-        if(isset($_FILES['upload_image']['name'])){
-            $upload_image_name = htmlspecialchars($_FILES['upload_image']['name'], ENT_QUOTES, 'UTF-8');
-            $image_path =  './img/'.htmlspecialchars($_FILES['upload_image']['name'], ENT_QUOTES, 'UTF-8');
-        } else {
-            $validation_error[] = "アップロードファイル名に問題があるか、未選択です"."<br>";
-        }
+                    $upload_image_tmp_name ='';
 
-        $upload_image_tmp_name ='';
+                    if(isset($_FILES['upload_image']['tmp_name'])){
+                        $upload_image_tmp_name = htmlspecialchars($_FILES['upload_image']['tmp_name'], ENT_QUOTES, 'UTF-8');
+                    }
 
-        if(isset($_FILES['upload_image']['tmp_name'])){
-            $upload_image_tmp_name = htmlspecialchars($_FILES['upload_image']['tmp_name'], ENT_QUOTES, 'UTF-8');
-        }
+                    if(!preg_match("/^[a-zA-Z0-9]+$/",$input_data)){
+                        $validation_error[] = "「画像名」が半角英数字以外の形式になってるか、未入力です。"."<br>";
+                    }
 
-        if(!preg_match("/^[a-zA-Z0-9]+$/",$input_data)){
-            $validation_error[] = "「画像名」が半角英数字以外の形式になってるか、未入力です。"."<br>";
-        }
-
-        $file=pathinfo($image_path);
-        $filetype=$file["extension"];
-            
-        if(!($filetype === "jpeg")){
-                if(!($filetype === "png")){
-                    $validation_error[] = "拡張子がJPEGまたはPNG以外の形式になっています"."<br>";
-                } 
-        }
-    
-
-        if(isset($_POST["id_value"])){
-            $number = htmlspecialchars($_POST["id_value"], ENT_QUOTES, 'UTF-8');
-        }
-
-// バリデーションチェックOKならSQL文(insert文)送る(➀各データの登録時)
-           
-        if (empty($validation_error) ){
-
-                $db = new mysqli($host, $login_user, $password, $database);
-                $db->set_charset("utf8");
-
-                $create_date = date('Ymd');
-                $update_date = date('Ymd');
-                $insert = "INSERT INTO gallery (image_name, public_flg, create_date, update_date,image_path) VALUES ('$input_data','0',".$create_date.",".$update_date.",'$image_path');";
-
-                if($result=$db->query($insert)){
-                    $save = 'img/'.basename($upload_image_name);
-                    move_uploaded_file($upload_image_tmp_name,$save);
-                    $str = "更新成功しました";
-                    print "<span class='msg'>$str</span><br>";
-                    $db->close();
-                } else {
-                    $str = "データベースに既に同じファイル名が存在している為か、その他の理由により更新失敗しました。"."<br>";
-                    print "<span class='msg'>$str</span><br>";
-                    $db->close();
+                    $file=pathinfo($image_path);
+                    $filetype=$file["extension"];
+                        
+                    if(!($filetype === "jpeg")){
+                            if(!($filetype === "png")){
+                                $validation_error[] = "拡張子がJPEGまたはPNG以外の形式になっています"."<br>";
+                            } 
+                    }
                 
-                }
 
-        } else {
-                foreach($validation_error as $err){
-                    print "<span class='msg'>$err</span><br>";
+            // バリデーションチェックOKならSQL文(insert文)送る(➀各データの登録時)
                     
-                }
-               
-        }
+                    if (empty($validation_error) ){
 
+                            $db = new mysqli($host, $login_user, $password, $database);
+                            $db->set_charset("utf8");
+
+                            $create_date = date('Ymd');
+                            $update_date = date('Ymd');
+                            $insert = "INSERT INTO gallery (image_name, public_flg, create_date, update_date,image_path) VALUES ('$input_data','0',".$create_date.",".$update_date.",'$image_path');";
+
+                            if($result=$db->query($insert)){
+                                $save = 'img/'.basename($upload_image_name);
+                                move_uploaded_file($upload_image_tmp_name,$save);
+                                $str = "更新成功しました";
+                                print "<span class='msg'>$str</span><br>";
+                                $db->close();
+                            } else {
+                                $str = "データベースに既に同じファイル名が存在している為か、その他の理由により更新失敗しました。"."<br>";
+                                print "<span class='msg'>$str</span><br>";
+                                $db->close();
+                            
+                            }
+
+                    } else {
+                            foreach($validation_error as $err){
+                                print "<span class='msg'>$err</span><br>";
+                                
+                            }
+                        
+                    }
+
+        }
 
 
         // ➂表示・非表示ボタンが押されたらデータベースに登録
-            if($_POST["btn"] == "表示する"){
+        if(isset($_POST["btn"])){
 
+            $number = htmlspecialchars($_POST["id_value"], ENT_QUOTES, 'UTF-8');
+
+            if($_POST["btn"] == "表示する"){
 
                 $db = new mysqli($host, $login_user, $password, $database);
                 $db->set_charset("utf8");
@@ -119,8 +118,9 @@
                 $db->query($update);
                 $db->close();
 
-            } else {
+            } 
 
+            if($_POST["btn"] == "非表示にする"){
                 $db = new mysqli($host, $login_user, $password, $database);
                 $db->set_charset("utf8");
                 $update = "UPDATE gallery SET public_flg = '1' WHERE image_id = ".$number.";";
@@ -130,25 +130,19 @@
                 $db->close();
 
             }
+
+
         }
+
       }
 
 // SQL文（select文）送る（➁登録後の画面表示）
 
         $db = new mysqli($host, $login_user, $password, $database);
         $db->set_charset("utf8");
-        // $a= "ALTER table gallery drop id";
-        // $db->query($a);
-        // $b = "ALTER table gallery add id int(11) primary key not null auto_increment first";
-        // $db->query($b);
-        // $c = "ALTER TABLE gallery AUTO_INCREMENT = 1";
-        // $db->query($c);
         $select = "SELECT * FROM gallery";
         $result = $db->query($select);
         
-
-
-    
 ?>
 
 
@@ -291,10 +285,9 @@
                 // ポイントはデータベースからidをもってきて、それをボタンにつける。ボタンに名前をつけてやる。それで、ボタンのid = データベースidで表示・非表示分ける。
                 // ポイントはデータベースから持ってきたflag情報をfetch_assocのループの中で場合分けして、各変数も全てここにいれる！
 
-                
-                // $j = 1;
-                $rows = $result->fetch_assoc();
-                foreach($rows as $row){
+               
+                while($row = $result->fetch_assoc()){
+
 
                     $get_img_url = $row["image_path"];
 
@@ -327,7 +320,6 @@
                         </div>
                     
             <?php
-                    // $j++;
                     }
                     $db->close();
             ?>
