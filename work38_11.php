6d48f5c93f67c9cf653b6fc8ebfcde00cc43820a
katoyutaka <kyupi13@gmail.com>
2023-02-28 15:35:49 +0900

    2/28

--- work38_11.php
+++ work38_11.php
@@ -1,63 +1,87 @@
 
 <?php
+
     session_start();
-    $_SESSION['err_flg'] = FALSE;
 
+    $validation_error = [];
+    
 
-    if ($_SESSION['err_flg'] === TRUE) {
+    if ($_SESSION['err_flg']) {
         echo "<p>ログインが失敗しました:正しいログインID（半角英数字）を入力してください。</p>";
     }
 
-    // $_SESSION['err_flg'] = FALSE;
+    $_SESSION['err_flg'] = False;
 
     //ログアウト処理がされた場合
     if (isset($_POST["logout"])) {
 
-        $_SESSION = [];
+        // セッション名を取得する
         $session = session_name();
-        setcookie($session, '', time() - 30, '/');
-        $message = "<p>ログアウトされました。</p>";
+        // セッション変数を削除
+        $_SESSION = [];
+
+        // セッションID（ユーザ側のCookieに保存されている）を削除
+        if (isset($_COOKIE[$session])) {
+       
+
+            // cookie削除
+            setcookie($session, '', time() - 30, '/');
+            $message = "<p>ログアウトされました。</p>";
+        }
 
     } else {
-        if ($_SESSION['err_flg'] = FALSE){
-            header('Location:work38_2.php');
+        // ログイン中のユーザーであるかを確認する
+        if (isset($_SESSION['login_id'])) {
+            // ログイン中である場合は、top.phpにリダイレクト（転送）する
+            header('Location: work38_22.php');
             exit();
         }
     }
 
 
+
     // <!-- ➃ -->
-    // <!-- cookie機能がセット（setcookie）があれば「$cookie_confirmation」の変数にcookieでゲットした値を格納する。入力されなかったら、「$cookie_confirmation」の変数は空欄。 --
+
     if(isset($_COOKIE["cookie_confirmation"]) === TRUE){
         $cookie_confirmation = "checked";
     } else {
         $cookie_confirmation = "";
     }
-    // <!-- cookie機能がセット（setcookie）があれば「$login_id」の変数にcookieでゲットした値を格納する。入力されなかったら、「$login_id」の変数は空欄。 -->
+
     if(isset($_COOKIE["login_id"]) === TRUE){
         $login_id = $_COOKIE["login_id"];
     } else {
         $login_id = "";
     }
-    ?>
+?>
+
+
+
 
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
    <meta charset="UTF-8">
-   <title>WORK38_1</title>
+   <title>WORK38_11</title>
+   <style>
+       .msg{
+            color:red;
+        }
+   </style>
 </head>
 <body>
-<?php
-    if (isset($message)) {
-        echo $message;
-    }  
 
-?>
+    <?php
+    // if (isset($validation_error)) {
+    //     foreach($validation_error as $err){
+    //         print "<span class='msg'>$err</span><br>";
+            
+    //     }
+    // }
+    ?>
 
-
-    <form method="post" action="work38_2.php">
+    <form method="post" action="work38_22.php">
         <label>ログインID</label><input type="text" name="login_id" value="<?php print $login_id;?>"><br>
         <label>パスワード</label><input type="password" name="password_id" value="<?php print $password_id;?>"><br>
         <input type="checkbox" name="cookie_confirmation" value="checked"<?php print $cookie_confirmation;?>>次回からログインIDの入力を省略する<br>
--- work38_2.php
+++ work38_2.php
@@ -1,132 +1,107 @@
 
-<?php
-    session_start();
+    <?php
+        session_start();       
+        
+        // cookieの保存期間を１日に設定
+        define("EXPIRATION_PERIOD", 1);
+        $cookie_expiration= time()+EXPIRATION_PERIOD*60*60*24;
 
-    // cookieの保存期間を１日に設定
-    define("EXPIRATION_PERIOD", 1);
-    define("DSN",'mysql:dbname=bcdhm_hoj_pf0001;host=mysql34.conoha.ne.jp');
-    define("LOGIN_USER",'bcdhm_hoj_pf0001');
-    define("PASSWORD",'Au3#DZ~G');
-    $cookie_expiration= time()+EXPIRATION_PERIOD*60*60*24;
+        // // ➁
+        // // もしチェックボックスがチェックされたら、「$cookie_confirmation」にデータを格納して、チェックされなかったら空欄を格納する。
+        // こちらは$_POSTに値があるならば、、（work37_1は$COOKIEに値があるならば）
 
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        // バリデーションチェック
-        $login_id= "";
-        if(!empty($_POST["login_id"])){
-            $login_id = htmlspecialchars($_POST['login_id'], ENT_QUOTES, 'UTF-8');
+        if(isset($_POST["cookie_confirmation"]) === TRUE){
+            $cookie_confirmation = $_POST["cookie_confirmation"];
+        } else {
+            $cookie_confirmation = "";
+        }
+        // もしログインIDが入力されたら、「$login_id」の変数に格納する。
+        if(isset($_POST["login_id"]) === TRUE){
+            $login_id = $_POST["login_id"];
             $_SESSION['login_id'] =  $login_id;
-
-        }else{
-            $validation_error[] = "ログインIDが未入力です。"."<br>"; 
-        }
-
-
-        $password_id= "";
-        if(!empty($_POST["password_id"])){
-            $password_id = htmlspecialchars($_POST['password_id'], ENT_QUOTES, 'UTF-8');
-            $_SESSION["password_id"] =  $password_id;
-
-        }else{
-            $validation_error[] = "パスワードが未入力です。"."<br>";
-        }
-
-
-        // バリデーションチェックをパスしたらデータベース接続
-        if (empty($validation_error) ){
-
-            try{
-                $db=new PDO($dsn,$login_user,$password);
-            } catch (PDOException $e){
-                $str = $e->getMessage();
-                print "<span class='msg'>$str</span><br>";
-                exit();
-            }
-
-            $sql = "SELECT user_id, user_name, password FROM user_table WHERE user_id = ".$login_id.';"';
+        } else {
+            $login_id = '';
             
-            if($result = $db->query($sql)){
-                $row = $result->fetch();
-
-                if($password_id===$row["password"]){
-                    $str1 ="<p>ログイン（擬似的）が完了しました</p>";
-                    $str2 = $row["user_name"]."さん、ようこそ！";
-
-                    print "<span class='msg'>$str1</span><br>";
-                    print "<span class='msg'>$str2</span><br>";
-
-                } else {
-                    $str = "パスワードが一致しないためログイン失敗しました。"."<br>";
-                    print "<span class='msg'>$str</span><br>";
-
-                }
-            }
+        }
+        // もしパスワードが入力されたら、「$password_id」の変数に格納する。
+        if(isset($_POST["password_id"]) === TRUE){
+            $password_id = $_POST["password_id"];
+            $_SESSION['password_id'] = $password_id;
+        } else {
+            $password_id = "";
+            
         }
 
-    }
-        
 
 
-    //     if(isset($_POST["cookie_confirmation"]) === TRUE){
-    //         $cookie_confirmation = $_POST["cookie_confirmation"];
-    //     } else {
-    //         $cookie_confirmation = "";
-    //     }
-        
-    //     if(isset($_POST["login_id"]) === TRUE){
-    //         $login_id = $_POST["login_id"];
-    //         $_SESSION['login_id'] =  $login_id;
-    //     } else {
-    //         $login_id = '';
-    //         $_SESSION['err_flg'] = true;
 
-    //     }
-
-    //     if(isset($_POST["password_id"]) === TRUE){
-    //         $password_id = $_POST["password_id"];
-    //     } else {
-    //         $password_id = '';
-    //         $_SESSION['err_flg'] = true;
-
-    //     }
-    
-
-    // // ➂
-    // if($cookie_confirmation === "checked"){
-    //     setcookie("login_id",$login_id,$cookie_expiration);
-    //     setcookie("cookie_confirmation",$cookie_confirmation,$cookie_expiration);
-    // } else {
-    //     setcookie("login_id","",time()-100);
-    //     setcookie("cookie_confirmation","",time()-100);
-    // }
+        // ➂
+        // 上記で、チェックがあれば（変数「$cookie_confirmation」に値が入れば）、cookie機能つけて、ないなら機能なしでcookie削除。
+        if($cookie_confirmation === "checked"){
+            setcookie("login_id",$login_id,$cookie_expiration);
+            setcookie("cookie_confirmation",$cookie_confirmation,$cookie_expiration);
+        } else {
+            setcookie("login_id","",time()-100);
+            setcookie("cookie_confirmation","",time()-100);
+        }
 
 
-    // if (!isset($_SESSION['login_id'])) {
-    //     // ログイン中ではない場合は、try55.phpにリダイレクト（転送）する
-    //     header('Location: work38_1.php');
-    //     exit();
-    // } else {
-    //     echo "<p>" . $_SESSION['login_id'] . "さん：ログイン中です。</p>";
-    // }
 
-    
-?>
+    ?>
+
+
+
+
+
+
+<!-- SQLデータベースからデータをもってきて、入力されたデータと照合させ、OK・NG判定をする部分 -->
+        <?php
+       
+            $dsn = 'mysql:dbname=bcdhm_hoj_pf0001;host=mysql34.conoha.ne.jp';
+            $login_user = 'bcdhm_hoj_pf0001'; 
+            $password = 'Au3#DZ~G'; 
+
+            $db=new PDO($dsn,$login_user,$password);
+            // $db->set_charset("utf8");
+            
+            // 「WHERE  user_id = $login_id」部分をカットして確認
+            $sql = "SELECT user_id, user_name, password FROM user_table WHERE  user_id = ".$login_id.';"';
+
+            
+            if(isset($password_id)){
+                    $result = $db->query($sql);
+                    
+                    $row = $result->fetch();
+
+
+                    if($password_id===$row["password"]){
+                        print "<p>ログイン（擬似的）が完了しました</p>";
+                        print $row["user_name"]."さん、ようこそ！";
+                    } else {
+                        print "ログインに失敗しました";
+                        $_SESSION['err_flg'] === TRUE;
+                    }
+        }
+
+        // if ($_SESSION['err_flg'] === TRUE){
+        //     // ログイン中ではない場合は、try55.phpにリダイレクト（転送）する
+        //     header('Location: work38_1.php');
+        //     exit();
+        // } else {
+        //     echo "<p>" . $_SESSION['login_id'] . "さん：ログイン中です。</p>";
+        // }
+        ?>
+
 <!DOCTYPE html>
-<html lang="ja">
-<head>
-   <meta charset="UTF-8">
-   <title>work38_2</title>
-   <style>
-       .msg{
-            color:red;
-        }
-   </style>
-</head>
-<body>
-    <form action="work38_1.php" method="post">
-        <input type="hidden" name="logout" value="logout">
-        <input type="submit" value="ログアウト">
-   </form>
-</body>
+    <html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>work38_2</title>
+    </head>
+    <body>
+            <form action="work38_1.php" method="post">
+                    <input type="hidden" name="logout" value="logout">
+                    <input type="submit" value="ログアウト">
+            </form>
+    </body>
 </html>
