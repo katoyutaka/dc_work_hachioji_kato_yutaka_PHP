effbed6bc923b1fa8793852338ce91f86e6ab79c
katoyutaka <kyupi13@gmail.com>
2023-03-27 15:30:24 +0900

    3/27

--- ec_site/login.php
+++ ec_site/login.php
@@ -1,9 +1,12 @@
 <?php
-       session_start();
+    //    session_start();
 
        define("DSN",'mysql:dbname=bcdhm_hoj_pf0001;host=mysql34.conoha.ne.jp');
        define("LOGIN_USER",'bcdhm_hoj_pf0001');
        define("PASSWORD",'Au3#DZ~G');
+       define("EXPIRATION_PERIOD", 1);
+
+       $cookie_expiration= time()+EXPIRATION_PERIOD*60*60*24;
         
        try{
             $db=new PDO(DSN,LOGIN_USER,PASSWORD);
@@ -12,6 +15,12 @@
             print $e->getMessage();
             exit();
         }
+
+       
+        $cookie_agree= "";
+        $login_user_name="";
+        $user_check="";
+        $cookie_display = "visible";
 
 ?>
 
@@ -22,77 +31,132 @@
             header('Location:membership_terms.php');
             exit();
         }
+        print "1";
 
 
 
-        if(isset($_POST["login_button"])){
+        // if(isset($_POST["login_button"])){
             
-            $validation_error = array();
+        //     $validation_error = array();
 
-            $sign_up_user_name ='';
-            if(!empty($_POST['sign_up_user_name'])){
+        //     $login_user_name ='';
+        //     if(!empty($_POST['login_user_name'])){
 
-                $sign_up_user_name = htmlspecialchars($_POST['sign_up_user_name'], ENT_QUOTES, 'UTF-8');
+        //         $login_user_name = htmlspecialchars($_POST['login_user_name'], ENT_QUOTES, 'UTF-8');
                
-                if(!preg_match("/^[a-zA-Z0-9]+$/",$sign_up_user_name)){
-                    $validation_error[] = "ユーザー名が半角英数字以外の形式になっています。"."<br>";
-                }
+        //         if(!preg_match("/^[a-zA-Z0-9]+$/",$login_user_name)){
+        //             $validation_error[] = "ユーザー名が半角英数字以外の形式になっています。"."<br>";
+        //         }
 
-            } else {
-                $validation_error[]="ユーザー名が未入力です"."<br>";
-            }
+        //     } else {
+        //         $validation_error[]="ユーザー名が未入力です"."<br>";
+        //     }
         
 
-            $sign_up_password_1="";
-            if(!empty($_POST['sign_up_password_1'])){
+        //     $sign_up_password_1="";
+        //     if(!empty($_POST['sign_up_password_1'])){
 
-                $sign_up_password_1 = htmlspecialchars($_POST['sign_up_password_1'], ENT_QUOTES, 'UTF-8');
+        //         $sign_up_password_1 = htmlspecialchars($_POST['sign_up_password_1'], ENT_QUOTES, 'UTF-8');
 
-                if(!preg_match("/^[a-zA-Z0-9]+$/",$sign_up_password_1)){
-                    $validation_error[] = "パスワードが半角英数字以外の形式になっています。"."<br>";
-                }
+        //         if(!preg_match("/^[a-zA-Z0-9]+$/",$sign_up_password_1)){
+        //             $validation_error[] = "パスワードが半角英数字以外の形式になっています。"."<br>";
+        //         }
 
-            } else {
-                $validation_error[]="パスワードが未入力です"."<br>";
-            }
+        //     } else {
+        //         $validation_error[]="パスワードが未入力です"."<br>";
+        //     }
 
         
 
     //バリデーションチェックOKならばデータベースと照合
-            if (empty($validation_error) ){
+            // if (empty($validation_error) ){
 
-                $select = "'SELECT * FROM ec_user_table WHERE user_name = '.$sign_up_user_name.'";
+            //     $select = "'SELECT * FROM ec_user_table WHERE user_name = '.$login_user_name.'";
 
-                if ($result = $db->query($select)) {
-                    while($row = $result->fetch()){
-                        print $row["password"];
-                    }
+            //     if ($result = $db->query($select)) {
+            //         while($row = $result->fetch()){
+            //             print $row["password"];
+            //         }
                         
     
-                    if($row["password"]===$sign_up_password_1){
-                        header('Location:complete_sign_up.php');
-                        exit();
+            //         if($row["password"]===$sign_up_password_1){
+            //             header('Location:complete_sign_up.php');
+            //             exit();
     
-                    } else {
-                        $str = "ユーザーIDまたはパスワードが一致しません";
+            //         } else {
+            //             $str = "ユーザーIDまたはパスワードが一致しません";
                            
-                    }
+            //         }
 
-                }
+            //     }
 
      
             
 
 
 
-            }else{
-                foreach($validation_error as $err){
-                    print "<span class='msg'>$err</span><br>";
+        //     }else{
+        //         foreach($validation_error as $err){
+        //             print "<span class='msg'>$err</span><br>";
                     
-                }
-            }
+        //         }
+        //     }
+        // }
+
         }
-    }
+
+        if(isset($_POST["cookie_agree"])){
+            $cookie_display = "hidden";
+
+            setcookie("user_check",$user_check,$cookie_expiration);
+            setcookie("login_user_name",$login_user_name,$cookie_expiration);
+        } 
+
+
+
+        if(isset($_POST["login_button"])){
+            print "2";
+        }
+
+        if(isset($_POST["login_user_name"])){
+            $login_user_name = $_POST["login_user_name"];
+            print $login_user_name;
+        }
+
+
+        if(isset($_POST["user_check"])){
+            $user_check = $_POST["user_check"];
+            print $user_check;
+        }
+
+                  
+        if($user_check==="checked"){
+            setcookie("user_check",$user_check,$cookie_expiration);
+            setcookie("login_user_name",$login_user_name,$cookie_expiration);
+        }
+
+
+        if(isset($_COOKIE["login_user_name"])){
+            $login_user_name = $_COOKIE["login_user_name"];
+        } else {
+            $login_user_name = "";
+        }
+
+
+        if(isset($_COOKIE["user_check"])){
+            $user_check = "checked";
+        } else {
+            $user_check = "";
+        }
+
+
+
+
+
+
+        
+        
+    // }
 
 
 ?>
@@ -309,53 +373,38 @@
                 .cookie-consent {
                     margin: 0 auto;
                     display: flex;
-                    justify-content: center;
-                    align-items: center;
+                    /* justify-content: center;
+                    align-items: center; */
                     position: fixed;
                     bottom: 0;
                     width: 100%;
                     font-size: 14px;
                     color: #fff;
                     background: rgba(0,0,100,0.5);
-                    padding: 1.2em;
-                    margin:0 auto;
-                    height: 180px;
+                    padding-top:40px;
+                    padding-bottom:40px;
+                    padding-left:400px;
+                    height: 160px;
                     font-family: system-ui;
                     letter-spacing: 2px;
                     color:white;
-                    
                 }
 
-                .cookie-consent.is-show {
-                    visibility: visible;
-                }
                 .policy-link, :link, :visited{
                     color:white;
                     font-family: system-ui;
                     letter-spacing: 2px;
                 }
-                .cookie-agree {
-                    color: #fff;
-                    background: dodgerblue;
-                    padding: .5em 1.5em;
+                .cookie_agree {
+                    color: white;
+                    background-color: #000099;
+                    padding:10px 30px;
                     margin-left: 20px;
+                    font-family: system-ui;
+                    letter-spacing: 2px;
                 }
-                .cookie-agree:hover{
+                .cookie_agree:hover{
                     cursor: pointer;
-                }
-
-                .cc-hide1 {
-                    display: none;
-                }
-
-                @keyframes hide {
-                    from {
-                        opacity: 1;
-                    }
-                    to {
-                        opacity: 0;
-                        visibility: hidden;
-                    }
                 }
 
 
@@ -385,38 +434,30 @@
         <div class="sub_wrapper">
             <p class="label_user1">Login</p>
                <div class="sub_container1">
-                 <p class="label_user2">Membership  <span>会員</span></p>
-                 <p class="text">【 重要なお知らせ 】<br>リニューアル以前に会員登録をされたお客様は、<br>はじめてログインする際、パスワードの再設定が必要です。 </p>
-                 <p class="text">【 LINE会員様へ 】<br>オンラインショップ会員と連動しておりません。<br>オンラインショップをご利用の際には新規会員登録をお願い致します。</p>
-               
+                    <p class="label_user2">Membership  <span>会員</span></p>
+                    <p class="text">【 重要なお知らせ 】<br>リニューアル以前に会員登録をされたお客様は、<br>はじめてログインする際、パスワードの再設定が必要です。 </p>
+                    <p class="text">【 LINE会員様へ 】<br>オンラインショップ会員と連動しておりません。<br>オンラインショップをご利用の際には新規会員登録をお願い致します。</p>
+                
+                    <form method="post" action="">
+                        <div class="third_wrapper">
+                                <div class="form_container">
+                                    <p class="sub_label1">ユーザー名</p>
+                                        <input type="text" class="user_name_form" name="login_user_name" value="<?php print $login_user_name ;?>">
+                                </div>
 
-                 <div class="third_wrapper">
-                    <div class="form_container">
-                        <p class="sub_label1">ユーザー名</p>
-                        <input type="text" class="user_name_form" name="sign_up_user_name">
-                    </div>
-
-                    <div class="form_container">
-                        <p class="sub_label2">パスワード</p>
-                        <input type="password" class="password_name_form" name="sign_up_password_1" >
-                        <br>
-                        <div class="checkbox">
-                            <input type="checkbox"  class="cookie_confirmation" name="cookie_confirmation" value="checked" <?php print $cookie_check; ?>>ユーザ名を記憶する
-                        </div>
+                                <div class="form_container">
+                                    <p class="sub_label2">パスワード</p>
+                                    <input type="password" class="password_name_form" name="sign_up_password_1" >
+                                    <br>
+                                    <div class="checkbox">
+                                        <input type="checkbox"  class="user_check" name="user_check" value="checked" <?php print $user_check; ?>>ユーザ名を記憶する
+                                    </div>
+                                </div>
+                            </div>
                         
-                    </div>
+                            <input type="submit" class="login_button"  name="login_button" value="ログインする">
+                    </form>
 
-                </div>
-
-                <form method="post" action="">
-                        <input type="submit" class="login_button"  name="login_button" value="ログインする">
-                </form>
-                
-                
-                
-               
-               
-               
                 </div>
 
                <div class="sub_container2">
@@ -435,17 +476,18 @@
      </div>
 
      
-     <div class="cookie-consent">
+     <div style="visibility:<?php print $cookie_display;?>;"class="cookie-consent">
         <div class="cookie-text">
             当サイトはクッキー(cookie)を使用します。クッキーはサイト内の一部の機能および、<br>
             サイトの使用状況の分析からマーケティング活動に利用することを目的としています。<br>
             <span class="policy-link">
-                <a href="privacy.php" target="_blank" >「プライバシーポリシーはこちらから」</a>
+                <a href="privacy.php">「プライバシーポリシーはこちらから」</a>
             </span>
         </div>
 
-        <div class="cookie-agree">同意する</div>
-
+        <form method="post" action="">
+            <input type="submit" class="cookie_agree" name="cookie_agree" value="同意する">
+        </form>
      </div>
 
 </body>
