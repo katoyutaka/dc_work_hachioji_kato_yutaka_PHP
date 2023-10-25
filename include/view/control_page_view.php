4f653f670643de1b0a5d08d10715f04f6641aa08
katoyutaka <kyupi13@gmail.com>
2023-10-25 13:47:41 +0900

    10/25

--- include/view/control_page_view.php
+++ include/view/control_page_view.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>商品管理ページ</title>
+    <title>商品管理</title>
 
 
     <style>
